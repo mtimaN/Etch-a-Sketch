1e09a5dc7848b0616978837124dc610d5b3e6f53
Matei Mantu <matei.mantu@stud.acs.upb.ro>
2023-09-18 14:59:13 +0300

    Added Rainbow and Shadow effect: bug known in Shadow Effect

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 var canvas_size = 512;
 var size = 16;
+
 function clearGrid(size) {
     let pixels = document.getElementsByClassName("pixel");
     for (let i = 0; i < size * size; ++i) {
         pixels[0].remove();
-    } 
+    }
 }
 
 function clearBoard(size) {
@@ -29,10 +30,46 @@ function createGrid(size) {
         div.style.height = `${canvas_size / size}px`;
         div.style.width = `${canvas_size / size}px`;
         div.className = "pixel";
+        div.style.backgroundColor = "#ffffff";
         canvas.appendChild(div);
         div.onmouseover = function() {
             this.style.backgroundColor = 'black';
         };
+    }
+}
+
+function shadow() {
+    clearBoard(size);
+    let pixels = document.getElementsByClassName("pixel");
+    for (let i = 0; i < pixels.length; ++i) {
+        pixels[i].onmouseover = function() {
+            let r = Number(pixels[i].style.backgroundColor.substring(4, 7));
+            let g = Number(pixels[i].style.backgroundColor.substring(9, 12));
+            let b = Number(pixels[i].style.backgroundColor.substring(14, 17));
+            r -= 26;
+            g -= 26;
+            b -= 26;
+            if (r < 0)
+                r = 0;
+            if (g < 0)
+                g = 0;
+            if (b < 0)
+                b = 0;
+            pixels[i].style.backgroundColor = "#" + (r << 16 | g << 8 | b).toString(16);
+            console.log(r, g, b);
+        }
+    }
+}
+
+function rainbow() {
+    clearBoard(size);
+    let pixels = document.getElementsByClassName("pixel");
+    console.log(pixels.length);
+    for (let i = 0; i < pixels.length; ++i) {
+        pixels[i].onmouseover = function() {
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            this.style.backgroundColor = "#" + randomColor;
+        }
     }
 }
 
